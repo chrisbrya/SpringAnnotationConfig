7cd05c06f29d10a8a27384f79b20e0766dc4e6bd
Chrisbry <c.bryantcf@gmail.com>
2022-09-29 10:58:14 -0400

    Annotation Config

---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Phone {
+
+    private String mob;
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "mob='" + mob + '\'' +
+                '}';
+    }
+}
